42f39cbb476f294156cb02813c1909d53016a1f7
krishna thakur <115836360+krsnaimp@users.noreply.github.com>
2024-06-22 23:38:22 +0530

    initial commit

--- src/main/kotlin/com/iauth/Application.kt
+++ src/main/kotlin/com/iauth/Application.kt
@@ -2,14 +2,26 @@ package com.iauth
 
 import com.iauth.plugins.*
 import io.ktor.server.application.*
+import org.litote.kmongo.coroutine.coroutine
+import org.litote.kmongo.reactivestreams.KMongo
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
+@Suppress("unused")
 fun Application.module() {
+
+    val mongoPW = System.getenv("MONGO_PW")
+    val dbName = "ktor-auth"
+    val db = KMongo.createClient(
+        connectionString = "mongodb+srv://krishna:$mongoPW@cluster0.kg7w2f6.mongodb.net/$dbName?retryWrites=true&w=majority&appName=Cluster0"
+    ).coroutine
+        .getDatabase(dbName)
+
     configureMonitoring()
     configureSerialization()
     configureSecurity()
     configureRouting()
 }
+
